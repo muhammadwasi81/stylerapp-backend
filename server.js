d7d59de28a77d06c8942fd571440d7acaac7c042
muhammadwasi81 <wasiarain819@gmail.com>
2023-07-13 01:29:11 +0500

    remove: unused code

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const colors = require('colors');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const { errorHandler, notFound } = require('./middleware/errorMiddleware');
-const morgan = require('morgan');
 require('dotenv').config();
 
 connectDB();
@@ -13,30 +12,16 @@ app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.json({ limit: '50mb' }));
 app.use(cors());
 
-if (process.env.NODE_ENV !== 'production') {
-  app.use(morgan('dev'));
-}
-
 // routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/delivery', require('./routes/deliveryRoutes'));
 app.use('/api/payments', require('./routes/paymentRoutes'));
 app.use('/api/products', require('./routes/productRoutes'));
 
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.get('/', (req, res) => {
+  res.send('API is running....');
+});
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
 app.use(errorHandler);
 app.use(notFound);
 
