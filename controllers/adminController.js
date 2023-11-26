24637a0b31a993571564aa56953c21f826a0a984
muhammadwasi81 <wasiarain819@gmail.com>
2023-11-26 16:59:31 +0500

    create dashborad stats api

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -28,4 +28,23 @@ const dashboardData = async (req, res) => {
   }
 };
 
-module.exports = { dashboardData };
+const dashboardStats = async (req, res) => {
+  try {
+    const totalUsers = await User.countDocuments({ isAdmin: false });
+    const totalDeliveries = await Delivery.countDocuments({});
+    const totalProducts = await Product.countDocuments({});
+    return res.send({
+      data: {
+        totalUsers,
+        totalDeliveries,
+        totalProducts,
+      },
+      message: "Data fetched successfully",
+      status: true,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error, status: false });
+  }
+};
+module.exports = { dashboardData, dashboardStats };
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const { dashboardData } = require("../controllers/adminController");
+const {
+  dashboardData,
+  dashboardStats,
+} = require("../controllers/adminController");
 const router = express.Router();
 
 router.route("/dashboard_data").get(dashboardData);
+router.route("/dashboard_stats").get(dashboardStats);
 
 module.exports = router;
